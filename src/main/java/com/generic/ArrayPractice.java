7cdd8679e027aba363eee1151d6ed97a347d766d
studentqa2021 <sarowerhome@gmail.com>
2021-07-19 18:50:46 -0400

    Added something new

---
+++ src/main/java/com/generic/ArrayPractice.java
@@ -0,0 +1,15 @@
+package com.generic;
+
+import java.util.Arrays;
+
+public class ArrayPractice {
+	
+	public static void main(String[] args) {
+		int [] age= {20,40,60,70};
+		System.out.println(Arrays.toString(age));
+		System.out.println(age[1]);
+		
+		
+	}
+
+}
